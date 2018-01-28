517d140d7e3dc78f7648e8230b695296df124352
Naveen <programmingwithnaveen@gmail.com>
2018-01-28 01:33:03 -0800

    added new java examples

--- src/com/pwn/stream/StreamToFilter.java
+++ src/com/pwn/stream/StreamToFilter.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package com.pwn;
+package com.pwn.stream;
 
 import java.util.stream.Stream;
 
@@ -14,12 +14,13 @@ public class StreamToFilter {
 	/**
 	 * @param args
 	 */
+	
 	public static void main(String[] args) {
 		Stream<String> fruitStream = Stream.of("apple", "Orange", "mango", "banana", "orange");
 		fruitStream.filter(s -> s.contains("a"))
-		.map(String::toUpperCase)
-		.sorted()
-		.forEach(System.out::println);
+				   .map(String::toUpperCase)
+				   .sorted()
+				   .forEach(System.out::println);
 	}
 
 }
